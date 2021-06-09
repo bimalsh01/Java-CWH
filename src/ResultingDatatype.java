0f4a5e4f050f8b9ebcee47ffc581614d7e777a0e
motivator29 <motivator291@gmail.com>
2021-06-09 09:52:24 +0545

    Increment and Decrement Operator

--- src/ResultingDatatype.java
+++ src/ResultingDatatype.java
@@ -1,5 +1,15 @@
 public class ResultingDatatype {
     public static void main(String[] args) {
-        int a = 654 + 6;
+
+        int a = 654 + 6; //Result is itiger
+        float b = 6.54f + a;
+        System.out.println(b);
+
+        // Increment operator and decrement operator
+        int i = 55;
+        int j = i++; // Last ++ means first assigned and increment
+        int k = ++i; //First ++ means value increases first
+        System.out.println(j); //55
+        System.out.println(k); //56
     }
 }
